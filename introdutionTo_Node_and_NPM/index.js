ea5547418f2e4c2edaaf2419496bb03c0241a1dc
walwizard <goneisgone@hotmail.com>
2023-02-13 13:45:56 -0300

    Writting and Reading files with NODE

--- introdutionTo_Node_and_NPM/index.js
+++ introdutionTo_Node_and_NPM/index.js
@@ -1,2 +1,12 @@
-const hello = "Hello World";
-console.log(hello);
+const fs = require("fs");
+// const hello = "Hello World";
+// console.log(hello);
+
+const textIn = fs.readFileSync('./text/input.txt', 'utf-8');
+
+// console.log(textIn);
+
+const textOut = `This is whta we know about avocado: \n ${textIn} \n Created on ${Date.now()}`;
+
+fs.writeFileSync('./text/output.txt', textOut);
+console.log('File Written!');
