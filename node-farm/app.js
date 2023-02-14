3c31fbf9b7277cad4d9057990c665eda1ee8168f
walwizard <goneisgone@hotmail.com>
2023-02-13 22:02:14 -0300

    Using part modules on the code

--- node-farm/app.js
+++ node-farm/app.js
@@ -2,11 +2,10 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 const slugify = require('slugify');
-const replaceTemplate = require('./modules/replaceTemplate');
-
+const replaceTemplate = require('./modules/replaceTemplate')
 
 // server
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8");
 
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
 
@@ -16,15 +15,15 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
-const slugs = dataObj.map(el => slugify(el.productName, {lower: true}))
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }))
 
-console.log(slugify('Fresh Avocados', {lower: true}));
+console.log(slugify('Fresh Avocados', { lower: true }));
 
 console.log(slugs);
 
 const server = http.createServer((req, res) => {
-    
-    const {query, pathname} = url.parse(req.url, true);
+
+    const { query, pathname } = url.parse(req.url, true);
 
     //Overview Page
     if (pathname === '/' || pathname === '/overview') {
@@ -38,7 +37,7 @@ const server = http.createServer((req, res) => {
 
         //Products Page
     } else if (pathname === '/product') {
-        
+
         res.writeHead(404, { 'Content-type': 'text/html' });
         const product = dataObj[query.id];
         const output = replaceTemplate(tempProduct, product)
