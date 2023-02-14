707f313ffa037c229328acf1ac0953c09d97939e
walwizard <goneisgone@hotmail.com>
2023-02-13 21:34:56 -0300

    Using part modules on the code

--- node-farm/app.js
+++ node-farm/app.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
-
+const slugify = require('slugify');
 const replaceTemplate = require('./modules/replaceTemplate');
+
 
 // server
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
@@ -15,6 +16,11 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
+const slugs = dataObj.map(el => slugify(el.productName, {lower: true}))
+
+console.log(slugify('Fresh Avocados', {lower: true}));
+
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
     
