872fe87e4fb7cb415ac1984c0e19bac6710a6383
walwizard <goneisgone@hotmail.com>
2023-02-20 20:09:43 -0300

    Bootcamp Node.js Fundamentals Foundations and Environment Setup

---
+++ Node.js-Fundamentals-Foundations-and-Environment-Setup/app/App.js
@@ -0,0 +1,7 @@
+const mission = 'learn';
+
+if (mission == 'learn') {
+    console.log('time to wrtie some node code!');
+} else {
+    console.log(`Is ${mission} really more fun?`);
+}
---
+++ Node.js-Fundamentals-Foundations-and-Environment-Setup/intro/hello.js
@@ -0,0 +1,5 @@
+function findMEaning(){
+    return 42;
+}
+
+console.log(findMEaning());
