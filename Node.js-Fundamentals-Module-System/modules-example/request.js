84924a40344c68b79daad738f0acc321fa0b0c5d
walwizard <goneisgone@hotmail.com>
2023-02-27 16:58:52 -0300

    Creating ur  Own Module

---
+++ Node.js-Fundamentals-Module-System/modules-example/request.js
@@ -0,0 +1,13 @@
+function encrypt(data) {
+    return 'encrypted data';
+}
+
+function send(url,data) {
+    const encryptedData = encrypt(data);
+    console.log(`Encrypting ${encryptedData} to ${url}`);
+}
+
+
+module.exports = {
+    send,
+}
---
+++ Node.js-Fundamentals-Module-System/modules-example/response.js
@@ -0,0 +1,11 @@
+function decrypt(data) {
+    return 'descrypted data'
+}
+
+function read(){
+    return decrypt('data');
+}
+
+module.exports = {
+    read,
+}
