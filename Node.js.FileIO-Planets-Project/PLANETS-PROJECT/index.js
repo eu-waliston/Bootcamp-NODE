0697c67967fa339e41027832a6c373bb42b94509
walwizard <goneisgone@hotmail.com>
2023-03-03 13:48:14 -0300

    Planets Projetcs - Finding Habitable Planets

--- Node.js.FileIO-Planets-Project/PLANETS-PROJECT/index.js
+++ Node.js.FileIO-Planets-Project/PLANETS-PROJECT/index.js
@@ -1,14 +1,19 @@
 const fs = require("fs");
 const {parse} = require("csv-parse");
 
-const results = [];
+const habitablePlanets = [];
+
+function isHabitablePlane(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED' && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11 && planet['koi_prad'] < 1.6;
+}
 
 fs.createReadStream("./kepler_data.csv").pipe(parse({
     comment: '#',
     columns: true,
 })).on('data', (data) => {
-    results.push(data)
+    if(isHabitablePlane(data)) {
+        habitablePlanets.push(data)
+    }
 }).on('end', () => {
-    console.log(results);
-    console.log('done 🔥');
+    console.log(`${habitablePlanets.length} habitable planets found!`);
 })
