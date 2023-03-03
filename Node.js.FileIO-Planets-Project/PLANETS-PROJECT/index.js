c0f8f32fd2b9ba2dcaa459abd6c02c7beef4168c
walwizard <goneisgone@hotmail.com>
2023-03-03 13:30:51 -0300

    Planets Projetcs - Parsing Our Planets Data

--- Node.js.FileIO-Planets-Project/PLANETS-PROJECT/index.js
+++ Node.js.FileIO-Planets-Project/PLANETS-PROJECT/index.js
@@ -1,13 +1,14 @@
-const parse = require("csv-parse");
 const fs = require("fs");
+const {parse} = require("csv-parse");
 
 const results = [];
 
-fs.createReadStream('./kepler_data.csv').on('data', (data) => {
-    results.push(data);
+fs.createReadStream("./kepler_data.csv").pipe(parse({
+    comment: '#',
+    columns: true,
+})).on('data', (data) => {
+    results.push(data)
 }).on('end', () => {
     console.log(results);
-    console.log("done!");
+    console.log('done 🔥');
 })
-
-// parse();
