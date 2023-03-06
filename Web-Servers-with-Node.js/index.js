9c300b4298ad62faa7b23294d67a37e42327fe4e
walwizard <goneisgone@hotmail.com>
2023-03-06 14:01:22 -0300

    Web Server With Node - HTTP API's and Routing

--- Web-Servers-with-Node.js/index.js
+++ Web-Servers-with-Node.js/index.js
@@ -3,14 +3,42 @@ const http = require("http");
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'application/json'
-    });
 
-    res.end(JSON.stringify({
-        id: 1,
-        name: 'Sir Isaac Newton',
-    }));
+    if (req.url === '/friends') {
+        // res.writeHead(200, {
+        //     'Content-Type': 'application/json'
+        // });
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json')
+        res.end(JSON.stringify({
+            id: 1,
+            name: 'Sir Isaac Newton',
+        }));
+    } else if (req.url === '/messages') {
+        res.setHeader('Content-Type', 'text/html')
+
+        res.write('<html>')
+        res.write('<body>')
+        res.write('<li> Hello Isaac! </li>')
+        res.write('<li> What are your thoughts about astronomy?</li>')
+        res.write('</html>')
+        res.write('</body>')
+        res.end();
+    } else {
+        res.statusCode = 500;
+
+        res.setHeader('Content-Type', 'text/html')
+
+        res.write('<html>')
+        res.write('<body>')
+        res.write('<h1>404 - PAGE NOT FOUND</h1>')
+        res.write('</html>')
+        res.write('</body>')
+        res.end();
+
+    }
+
+
 });
 
 
