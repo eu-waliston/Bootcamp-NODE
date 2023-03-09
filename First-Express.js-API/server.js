36c07f2583cf3b4e2783dd149cdf5f78cbd7169c
walwizard <goneisgone@hotmail.com>
2023-03-09 14:05:19 -0300

    Route Parameters

--- First-Express.js-API/server.js
+++ First-Express.js-API/server.js
@@ -1,17 +1,36 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const friends = [
+    {
+        id: 0,
+        name: 'Albert Eisntein',
+    },
+    {
+        id: 1,
+        name: 'Isaac Newton',
+    }
+];
 
 
 app.get('/', (req,res) => {
-    res.status(200).send({
-        id: 1,
-        name: 'Sir Isaac Newton'
-    })
+    res.status(200).json(friends)
 })
 
 app.get('/messages', (req,res) => {
     res.status(200).send('<h1>Hello Albert!</h1>')
+})
+
+app.get('/friends/:friendID' , (req,res) => {
+    const friendID = Number(req.params.friendID);
+    const friend = friends[friendID];
+    if(friend){
+        res.status(200).json(friend)
+    } else {
+        res.status(404).json({
+            error: "Friend does not exists"
+        })
+    }
 })
 
 app.post('/messages', (req,res) => {
