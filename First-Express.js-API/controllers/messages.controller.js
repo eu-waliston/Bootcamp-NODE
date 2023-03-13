2fb62e6a3f5a067159e405d43341dfc6a3f96555
walwizard <goneisgone@hotmail.com>
2023-03-13 19:35:40 -0300

    Layouts and Separation of Concerns

--- First-Express.js-API/controllers/messages.controller.js
+++ First-Express.js-API/controllers/messages.controller.js
@@ -1,7 +1,11 @@
 const path = require("path");
 
 function getMessages(req, res) {
-  res.sendFile(path.join(__dirname, '..', 'public', 'images', 'skimountain.jpg'));
+  res.render('messages', {
+    title: 'Messages to my Friends',
+    friend: 'Elon Musk',
+  })
+  // res.sendFile(path.join(__dirname, '..', 'public', 'images', 'skimountain.jpg'));
   // res.status(200).send('<h1>Hello Albert!</h1>')
 }
 
