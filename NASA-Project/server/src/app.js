d957d0d98780cc350aec1660364c201229a99fa4
walwizard <goneisgone@hotmail.com>
2023-03-21 09:37:17 -0300

    Nasa Project - CORS Midleware

--- NASA-Project/server/src/app.js
+++ NASA-Project/server/src/app.js
@@ -1,11 +1,23 @@
 const express = require("express");
+const cors = require("cors");
 
 const planetsRouter = require("./routes/planets/planets.router");
 
+let whitelist = ['https://locahost:3000']
 
+let corsOption = {
+    origin: function(origin, callback) {
+        if(whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not Allowed by CORS'))
+        }
+    }
+}
 
 const app = express();
 
+app.use(cors(corsOption));
 app.use(express.json());
 app.use(planetsRouter);
 
