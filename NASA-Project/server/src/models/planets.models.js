11c59e1ff74a0ebc664071059d3ec7b175babdf8
walwizard <goneisgone@hotmail.com>
2023-03-23 21:11:04 -0300

    NASA Project - The Planets Model

--- NASA-Project/server/src/models/planets.models.js
+++ NASA-Project/server/src/models/planets.models.js
@@ -1,3 +1,29 @@
-const planets = [];
+const fs = require("fs");
+const {parse} = require("csv-parse");
 
-module.exports = planets;
+const habitablePlanets = [];
+
+function isHabitablePlane(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED' && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11 && planet['koi_prad'] < 1.6;
+}
+
+fs.createReadStream("./kepler_data.csv").pipe(parse({
+    comment: '#',
+    columns: true,
+})).on('data', (data) => {
+    if(isHabitablePlane(data)) {
+        habitablePlanets.push(data)
+    }
+}).on('end', () => {
+    console.log(`${habitablePlanets.length} habitable planets found!`);
+
+    console.log('\nAnd They are: \n');
+    console.log(habitablePlanets.map((planet) => {
+        return planet['kepler_name']
+    }));
+})
+
+
+module.exports = {
+    planets: habitablePlanets,
+}
