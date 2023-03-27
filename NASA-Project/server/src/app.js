8ac3de87a329dc44351199135006c350890eb4e7
walwizard <goneisgone@hotmail.com>
2023-03-27 20:17:01 -0300

    Nasa-Project, Logging Requests With Morgan

--- NASA-Project/server/src/app.js
+++ NASA-Project/server/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const path = require("path")
+const path = require("path");
+const morgan = require("morgan");
 
 const planetsRouter = require("./routes/planets/planets.router");
 
@@ -9,15 +10,17 @@ const app = express();
 app.use(cors({
     origin: 'http://localhost:3000',
 }));
+app.use(morgan('combined'));
+
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '..', 'public')))
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 
 app.use(planetsRouter);
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
 })
-
 
 module.exports = app;
 
