e5cf75231f0507999a3c12cb5f1011b1906deb20
walwizard <goneisgone@hotmail.com>
2023-04-07 19:25:17 -0300

    NASA Project - POST launches Validation for POST request

--- NASA-Project/server/src/routes/launches/launches.controller.js
+++ NASA-Project/server/src/routes/launches/launches.controller.js
@@ -1,24 +1,36 @@
-const { getAllLauches, addNewLaunch }  = require('../../models/launches.models');
+const { getAllLauches, addNewLaunch } = require("../../models/launches.models");
 
-function httpGetAllLaunches(req,res){
-    return res.status(200).json(getAllLauches());
+function httpGetAllLaunches(req, res) {
+  return res.status(200).json(getAllLauches());
 }
 
-function httpAddNewLaunch(req,res) {
+function httpAddNewLaunch(req, res) {
+  const launch = req.body;
 
-    const launch = req.body;
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.launchDate ||
+    !launch.destination
+  ) {
+    return res.status(400).json({
+      error: "Missing required launch property",
+    });
+  }
 
-    launch.launchDate = new Date(launch.launchDate);
+  launch.launchDate = new Date(launch.launchDate);
+  
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({
+        error: 'Invalid launch date',
+    });
+  }
 
-
-    addNewLaunch(launch);
-
-    return res.status(201).json(launch)
+  addNewLaunch(launch);
+  return res.status(201).json(launch);
 }
-
 
 module.exports = {
-    httpGetAllLaunches,
-    httpAddNewLaunch
-}
-
+  httpGetAllLaunches,
+  httpAddNewLaunch,
+};
