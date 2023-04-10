43ebfbdd6d93a5a7f329a6e9a4bafd5919990f89
walwizard <goneisgone@hotmail.com>
2023-04-10 20:09:40 -0300

    testing API endpoints with supertest GET

--- Testing-APIS/server/src/routes/launches/launches.test.js
+++ Testing-APIS/server/src/routes/launches/launches.test.js
@@ -1,9 +1,14 @@
+const request = require("supertest");
+const app = require("../../app");
+
 describe('Test Get /launches', () => {
-    test('It should respond with 200 success', () => {
-        const response = 200;
-        expect(response).toBe(200);
-    })
-})
+    test('It should respond with 200 success', async () => {
+        const response = await request(app)
+            .get('/launches')
+            .expect('Content-Type', /json/)
+            .expect(200);
+    });
+});
 
 describe('Test POST /launch' , () => {
     test('It should respond wiht 200 success', () => {
