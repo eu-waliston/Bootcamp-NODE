912e009c1d20e2648b095d5beaa00e146280e4d2
walwizard <goneisgone@hotmail.com>
2023-04-10 20:59:26 -0300

    testing API endpoints with supertest ERROR

--- Testing-APIS/server/src/routes/launches/launches.test.js
+++ Testing-APIS/server/src/routes/launches/launches.test.js
@@ -24,6 +24,13 @@ describe("Test POST /launch", () => {
     target: "Kepler-186 f",
   };
 
+  const launchDataWithInvalidDate = {
+    mission: "USS Enterprise",
+    rocket: "NCC 1701-D",
+    target: "Kepler-186 f",
+    launchDate: "zoot",
+  };
+
   test("It should respond wiht 201 created", async () => {
     const response = await request(app)
       .post("/launches")
@@ -33,13 +40,33 @@ describe("Test POST /launch", () => {
 
     const requestDate = new Date(completeLauchData.launchDate).valueOf();
     const responseDate = new Date(response.body.launchDate).valueOf();
-    
+
     expect(responseDate).toBe(requestDate);
 
     expect(response.body).toMatchObject(launchDataWithouthDate);
   });
 
-  test("It should catch missing require properties", () => {});
+  test("It should catch missing require properties", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(launchDataWithouthDate)
+      .expect("Content-Type", /json/)
+      .expect(400);
 
-  test("It should catch invalid dates", () => {});
+    expect(response.body).toStrictEqual({
+      error: "Missing required launch property",
+    });
+  });
+
+  test("It should catch invalid dates", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(launchDataWithInvalidDate)
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    expect(response.body).toStrictEqual({
+      error: "Invalid launch date",
+    });
+  });
 });
