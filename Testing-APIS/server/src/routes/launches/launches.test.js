e2efe2ffb1bf16c7476ae1c670b1b9dc3f7eb14d
walwizard <goneisgone@hotmail.com>
2023-04-10 20:33:12 -0300

    testing API endpoints with supertest POST

--- Testing-APIS/server/src/routes/launches/launches.test.js
+++ Testing-APIS/server/src/routes/launches/launches.test.js
@@ -1,23 +1,45 @@
 const request = require("supertest");
 const app = require("../../app");
 
-describe('Test Get /launches', () => {
-    test('It should respond with 200 success', async () => {
-        const response = await request(app)
-            .get('/launches')
-            .expect('Content-Type', /json/)
-            .expect(200);
-    });
+describe("Test Get /launches", () => {
+  test("It should respond with 200 success", async () => {
+    const response = await request(app)
+      .get("/launches")
+      .expect("Content-Type", /json/)
+      .expect(200);
+  });
 });
 
-describe('Test POST /launch' , () => {
-    test('It should respond wiht 200 success', () => {
+describe("Test POST /launch", () => {
+  const completeLauchData = {
+    mission: "USS Enterprise",
+    rocket: "NCC 1701-D",
+    target: "Kepler-186 f",
+    launchDate: "January 4, 2028",
+  };
 
-    })
+  const launchDataWithouthDate = {
+    mission: "USS Enterprise",
+    rocket: "NCC 1701-D",
+    target: "Kepler-186 f",
+  };
 
-    test('It should catch missing require properties', () => {
+  test("It should respond wiht 201 created", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(completeLauchData)
+      .expect("Content-Type", /json/)
+      .expect(201);
 
-    })
+    const requestDate = new Date(completeLauchData.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+    
+    expect(responseDate).toBe(requestDate);
 
-    test('It should catch invalid dates', () => {});
-})
+    expect(response.body).toMatchObject(launchDataWithouthDate);
+  });
+
+  test("It should catch missing require properties", () => {});
+
+  test("It should catch invalid dates", () => {});
+});
