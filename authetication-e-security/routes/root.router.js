dd4c978f4c4c62e2b69903c75ab6a3255547d382
walwizard <goneisgone@hotmail.com>
2023-05-03 17:32:54 -0300

    Bootcamp Node , Authentication with google and OAuth PT 2

--- authetication-e-security/routes/root.router.js
+++ authetication-e-security/routes/root.router.js
@@ -1,22 +1,37 @@
-const express = require('express');
-const path = require('path');
-
-const checkLoggedIn = require('../auth/checkLoggedIn');
+const express = require("express");
+const path = require("path");
+const passport = require("passport");
+const checkLoggedIn = require("../auth/checkLoggedIn");
 
 const root = express.Router();
 
-root.get('/secret', checkLoggedIn, (req,res) => {
-    res.send('Your personal secret value is 42!')
-})
+root.get("/secret", checkLoggedIn, (req, res) => {
+  res.send("Your personal secret value is 42!");
+});
 
-root.get('/auth/google', (req,res) => {})
+root.get("/auth/google", passport.authenticate('google', {
+    scope: ['email'],
+}));
 
-root.get('/auth/google/callback', (req,res) => {})
+root.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/failure",
+    successRedirect: "/",
+    session: false,
+  }),
+  (req, res) => {
+    console.log("Google called us back!");
+  }
+);
 
-root.get('/auth/logout', (req,res) => {})
+root.get("/auth/logout", (req, res) => {});
+root.get("/failure", (req, res) => {
+  return res.send("Failed to log in");
+});
 
-root.get('/',  (req,res) => {
-    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
-})
+root.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = root;
