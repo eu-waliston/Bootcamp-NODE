65fdbf51bea6fc71d43aca13ed42dc78a6080763
walwizard <goneisgone@hotmail.com>
2023-05-03 17:14:29 -0300

    Bootcamp Node , Authentication with google and OAuth PT 1

--- authetication-e-security/server.js
+++ authetication-e-security/server.js
@@ -2,20 +2,34 @@ const fs = require("fs");
 require("dotenv").config();
 const https = require("https");
 const express = require("express");
-const helmet = require('helmet');
+const helmet = require("helmet");
+const passport = require("passport");
+const { Strategy } = require("passport-google-oauth20");
 
 const root = require("./routes/root.router");
+const config = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+};
+
+const AUTH_OPTIONS = {
+  callbackURL: "/auth/google/callback",
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
+};
+
+function verifyCallback(accessToken, refreshToken, profile, done){
+  console.log('Google profile', profile);
+  done(null, profile);
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
 
 const app = express();
 
-const config = {
-  CLIENT_ID:  process.env.CLIENT_ID,
-  CLIENT_SECRET: process.env.CLIENT_SECRET
-};
-
 //middlewares
 app.use(helmet());
-
+app.use(passport.initialize());
 
 app.use("/", root);
 
