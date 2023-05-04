d3221915a55fd8ad2e243ab1ae28ec305ba2553b
walwizard <goneisgone@hotmail.com>
2023-05-03 21:33:14 -0300

    Bootcamp Node , Restricting aceess to endpoints

--- authetication-e-security/auth/checkLoggedIn.js
+++ authetication-e-security/auth/checkLoggedIn.js
@@ -1,5 +1,6 @@
 function checkLoggedIn(req, res, next) {
-  const isLoggedIn = true; //TODO
+  console.log('Current user is', req.user);
+  const isLoggedIn = rq.isAuthenticated() && req.user;
   if (!isLoggedIn) {
     return res.status(401).json({
       error: "You must log in!",
