ecdb4935c7304b40d95e9c39e9c1ded5c52e7535
walwizard <goneisgone@hotmail.com>
2023-05-24 20:58:50 -0300

    GrapgQL - Modularizing Large GraphQL Projects Schemas

---
+++ graphql/products/products.model.js
@@ -0,0 +1,13 @@
+module.exports = [
+  {
+    id: "redshoe",
+    description: "Red Shoe",
+    price: 42.12,
+  },
+
+  {
+    id: "bluejean",
+    description: "Blue Jeans",
+    price: 55.55,
+  },
+];
