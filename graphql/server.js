384c283b88ac3bd008f761df9bab16039636cb9d
walwizard <goneisgone@hotmail.com>
2023-05-31 07:21:55 -0300

    Booitcamp Node - GraphQl, Resolvers

--- graphql/server.js
+++ graphql/server.js
@@ -11,6 +11,19 @@ const typesArray = loadFilesSync(path.join(__dirname, "**/*.graphql"));
 
 const schema = makeExecutableSchema({
   typeDefs: typesArray,
+  resolvers: {
+    Query: {
+      products: async (parent, args, context, info) => {
+        console.log("Getting the products...");
+        const product =  await Promise.resolve(parent.products);
+        return product;
+      },
+      orders: (parent) => {
+        console.log("Geting the orders..");
+        return parent.orders;
+      },
+    }
+  }
 });
 
 const root = {
