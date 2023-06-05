b5485389455ed9468fd13a6f87ace249627e8848
walwizard <goneisgone@hotmail.com>
2023-06-05 20:34:14 -0300

    Exercise Add New Product Review

--- graphql/products/products.model.js
+++ graphql/products/products.model.js
@@ -42,9 +42,24 @@ function addNewProduct(id, description, price) {
   return newProduct;
 }
 
+function addNewProductReview(id, rating, comment) {
+  const matchedProduct = getProductById(id);
+
+  if(matchedProduct) {
+    const newProductReview = {
+      rating,
+      comment
+    }
+
+    matchedProduct.reviews.push(newProductReview);
+    return newProductReview;
+  }
+}
+
 module.exports = {
   getAllProducts,
   getProductsByPrice,
   getProductById,
-  addNewProduct
+  addNewProduct,
+  addNewProductReview
 };
