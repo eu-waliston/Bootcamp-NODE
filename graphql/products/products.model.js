3351a1cea65ba8f57dc54718b00557915388b67b
walwizard <goneisgone@hotmail.com>
2023-06-05 19:13:01 -0300

    Exercise Query Products By ID

--- graphql/products/products.model.js
+++ graphql/products/products.model.js
@@ -22,9 +22,14 @@ function getProductsByPrice(min, max) {
   });
 }
 
+function getProductById(id) {
+  return products.find((product) => {
+    return product.id === id;
+  })
+}
 
 module.exports = {
   getAllProducts,
   getProductsByPrice,
-
+  getProductById
 };
