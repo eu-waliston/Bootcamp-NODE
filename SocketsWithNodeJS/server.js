1bd8b91729e540499487a7618ec9717d6d64ca08
Waliston <goneisgone@hotmail.com>
2023-06-16 02:13:41 -0300

    Bootcamp Node - listenning for events in pong Server

--- SocketsWithNodeJS/server.js
+++ SocketsWithNodeJS/server.js
@@ -12,8 +12,19 @@ const PORT = 3000;
 server.listen(PORT);
 console.log(`Listen on PORT ${PORT}`);
 
+let readyPlayerCount = 0;
+
 io.on('connection', (socket) => {
   console.log('a user connected', socket.id);
+
+  socket.on('ready', () => {
+    console.log('Player ready', socket.id);
+
+    readyPlayerCount++;
+    if(readyPlayerCount === 2) {
+      //broadcast('startgame')
+    }
+  })
 })
 
 /*
